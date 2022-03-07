3722431576038aa9e12fb8791086a805fa8a967b
Ashok Modhave <ashok.modhave143@gmail.com>
2022-03-08 11:48:22 +1300

    add splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,24 @@
  * @flow strict-local
  */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, Text, View } from 'react-native'
 import { styles } from './App.styles'
 import { Deals } from './components/deals'
+import RNBootSplash from 'react-native-bootsplash'
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    }
+
+    init().finally(async () => {
+      await RNBootSplash.hide({ fade: true })
+      console.log('Bootsplash has been hidden successfully')
+    })
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       {/* <ScrollView contentInsetAdjustmentBehavior="automatic" nestedScrollEnabled={true}> */}
